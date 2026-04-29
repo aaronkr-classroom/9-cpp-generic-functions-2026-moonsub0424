b3de30dc53dcc7fa3f203d586a1677891268652d
ththfql <131600659+moonsub0424@users.noreply.github.com>
2026-04-29 17:10:29 +0900

    week9

---
+++ Chp9/grade.h
@@ -0,0 +1,20 @@
+﻿// grade.h
+#ifndef GUARD_grade_h
+#define GUARD_grade_h
+
+#include <list>
+#include <vector>
+#include "Student_info.h"
+
+using std::vector;
+
+bool fgrade(const Student_info&);
+bool pgrade(const Student_info&);
+
+double grade(double, double, double);
+double grade(double, double, vector<double>&);
+double grade(const Student_info&);
+
+list<Student_info> extract_fails(list<Student_info>&);
+
+#endif
---
+++ Chp9/median.h
@@ -0,0 +1,11 @@
+﻿// median.h
+#ifndef GUARD_median_h
+#define GUARD_median_h
+
+#include <vector>
+
+using std::vector;
+
+double median(vector<double>);
+
+#endif
